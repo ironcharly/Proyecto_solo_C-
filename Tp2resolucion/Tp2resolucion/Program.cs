b8ceef3ab35337a84f8ccd03b4e2fdd88fea164f
Spinetta_Carlos <spinetta.carlosa@gmail.com>
2026-04-27 20:02:12 -0300

    Proyecto
    Armamos el proyecto base

---
+++ Tp2resolucion/Tp2resolucion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tp2resolucion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
